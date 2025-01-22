2128c1842311de9aab24ae7b79dc5a3d95199d17
elky <elky84@gmail.com>
2025-01-22 23:31:30 +0900

    설정 파일 규칙대로 응답

--- Web/Service/SlackBotService.cs
+++ Web/Service/SlackBotService.cs
@@ -1,23 +1,28 @@
 ﻿using SlackNet;
 using SlackNet.Events;
 using SlackNet.WebApi;
+using Web.Common.Config;
 using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace Web.Service;
 
-public class SlackBotService(ISlackApiClient slack, ILogger<SlackBotService> log) : IEventHandler<MessageEvent>
+public class SlackBotService(ISlackApiClient slack, ILogger<SlackBotService> log, SlackBotConfig slackBotConfig) : IEventHandler<MessageEvent>
 {
     private readonly ILogger _log = log;
 
     public async Task Handle(MessageEvent slackEvent)
     {
-        if (slackEvent.Text?.Contains("ping", StringComparison.OrdinalIgnoreCase) == true)
+        if (string.IsNullOrEmpty(slackEvent.Text))
+            return;
+        
+        var keyValuePair = slackBotConfig.Keywords.FirstOrDefault(x => slackEvent.Text.Contains(x.Key, StringComparison.OrdinalIgnoreCase));
+        if(!string.IsNullOrEmpty(keyValuePair.Key))
         {
             _log.LogInformation("Received ping from {User} in the {Channel} channel", (await slack.Users.Info(slackEvent.User)).Name, (await slack.Conversations.Info(slackEvent.Channel)).Name);
             
             await slack.Chat.PostMessage(new Message
             {
-                Text = "pong",
+                Text = keyValuePair.Value,
                 Channel = slackEvent.Channel
             });
         }
